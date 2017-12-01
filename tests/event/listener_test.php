af328c2aa7a441589568f121be16e61643543059
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-12-01 17:07:54 -0600

    Add unit tests

---
+++ tests/event/listener_test.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Hide Extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0
+ */
+
+namespace alfredoramos\hide\tests\event;
+
+use phpbb_test_case;
+use alfredoramos\hide\event\listener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @group event
+ */
+class listener_test extends phpbb_test_case
+{
+
+	public function test_instance()
+	{
+		$this->assertInstanceOf(
+			EventSubscriberInterface::class,
+			new listener
+		);
+	}
+
+	public function test_suscribed_events()
+	{
+		$this->assertSame(
+			['core.user_setup'],
+			array_keys(listener::getSubscribedEvents())
+		);
+	}
+}
