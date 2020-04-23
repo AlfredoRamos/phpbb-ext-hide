df0ac089dcd87d2a8049b7a4fe147caccdf871ed
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-04-22 20:38:34 -0500

    New translations posting.php (Portuguese, Brazilian)

---
+++ language/pt_br/posting.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Hide extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2017 Alfredo Ramos
+ * @license GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'HIDE' => 'Ocultar',
+	'HIDE_HELPLINE' => 'Uso: [hide]texto[/hide] ou [hide inline=1]texto[/hide]',
+	'HIDDEN_CONTENT' => 'Conteúdo oculto',
+	'HIDDEN_CONTENT_EXPLAIN' => 'Conteúdo exclusivo para usuários logados.'
+]);
